a8054d23bb40c6c2000f4eb500cd379e7fa8a169
Ug98 <iudjell98@gmail.com>
2024-08-02 17:04:55 +0300

    filter begging

--- forum/src/views/AllPosts.jsx
+++ forum/src/views/AllPosts.jsx
@@ -1,43 +1,64 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getAllPosts } from "../services/posts.service";
 import { useNavigate, useSearchParams } from "react-router-dom";
-import Comments from '../components/Comments';
+import Comments from "../components/Comments";
 
 export default function AllPosts() {
-  const [posts, setPosts] = useState([]);
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const search = searchParams.get('search') ?? '';
+    const [posts, setPosts] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    const [searchParams, setSearchParams] = useSearchParams();
+    const search = searchParams.get('search') ?? '';
+    const navigate = useNavigate();
+    const [category, setCategory] = useState('all');
 
-  console.log(posts);
+    useEffect(() => {
+        getAllPosts(search)
+            .then(posts => {
+                setPosts(posts);
+                filterPosts(posts, category);
+            })
+            .catch(error => alert(error.message));
+    }, [search]);
 
-  useEffect(() => {
-    getAllPosts(search)
-      .then(posts => setPosts(posts))
-      .catch(error => alert(error.message));
-  }, [search]);
+    useEffect(() => {
+        filterPosts(posts, category);
+    }, [category, posts]);
 
-  const setSearch = (value) => {
-    setSearchParams({
-      search: value,
-    });
-  }
+    const filterPosts = (posts, category) => {
+        if (category === 'all') {
+            setFilteredPosts(posts);
+        } else {
+            setFilteredPosts(posts.filter(posts => posts.category === category));
+        }
+    };
 
-  return (
-    <div>
-      <h1>Posts:</h1>
-      <label htmlFor="search"></label>
-      <input value={search} onChange={e => setSearch(e.target.value)} type="text" name="search" id="search" /><br/><br/>
-      {posts.length > 0
-      ? posts.map(t => (
-        <div key={t.id}>
-          <h3>{t.title}</h3>
-          <p>{t.content} <button onClick={() => navigate(`/posts/${t.id}`)}>See more</button></p>
-          <Comments postId={t.id} limit={3} />
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    };
+
+    return (
+        <div>
+            <h1>Posts:</h1>
+            <label htmlFor="search">Search: </label>
+            <input value={search} onChange={e => setSearchParams({ search: e.target.value })} type="text" name="search" id="search" /><br/><br/>
+            <label htmlFor="category">Category: </label>
+            <select value={category} onChange={handleCategoryChange} name="category" id="category">
+                <option value="all">All</option>
+                <option value="Soups">Soups</option>
+                <option value="Salads">Salads</option>
+                <option value="Main courses">Main courses</option>
+                <option value="Vegetarian">Vegetarian</option>
+            </select><br/><br/>
+            {filteredPosts.length > 0
+            ? filteredPosts.map(t => (
+                <div key={t.id}>
+                    <h3>{t.title}</h3>
+                    <p>{t.content} <button onClick={() => navigate(`/posts/${t.id}`)}>See more</button></p>
+                    <Comments postId={t.id} limit={3} />
+                </div>
+            ))
+            : 'No posts'
+            }
         </div>
-      ))
-      : 'No posts'
-      }
-    </div>
-  )
+    );
 }
